a890d0dca42017c28faadcf8a264817bfa340d4d
Ulugbek <ulugb3k270@gmail.com>
2024-05-22 13:16:41 +0500

    templates blog added

---
+++ components/right-sidebar/right-sidebar-settings/donation.setting.tsx
@@ -0,0 +1,25 @@
+import { ColorPicker } from '@/components/color-picker';
+import { useLocaleContext } from '@/context/locale.context';
+import { useBlogValues } from '@/hooks/useBlogValues';
+
+export const DonationSetting = () => {
+  const { t } = useLocaleContext();
+  const [textColor, setTextColor] = useBlogValues('color');
+  const [backgroundColor, setBackgroundColor] =
+    useBlogValues('backgroundColor');
+
+  return (
+    <div className="px-4 flex flex-col space-y-3">
+      <ColorPicker
+        title={t('settings.properties.text_color')}
+        color={textColor}
+        onChange={(color) => setTextColor(color.hex)}
+      />
+      <ColorPicker
+        title={t('settings.properties.button_background')}
+        color={backgroundColor}
+        onChange={(color) => setBackgroundColor(color.hex)}
+      />
+    </div>
+  );
+};
