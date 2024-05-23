1c5cb17227013be305438f1e86b43e1c3dedeabd
Ulugbek <ulugb3k270@gmail.com>
2024-05-23 20:24:35 +0500

    dnd error fixed

--- components/phone.tsx
+++ components/phone.tsx
@@ -9,7 +9,6 @@ import { Template } from './template';
 
 export const Phone = () => {
   const { currentScreenSize, template, setTemplate } = usePhoneContext();
-
   const onDragEnd = (result: DropResult) => {
     const destinationIndex = result.destination?.index || 0;
     const sourceIndex = result.source.index;
@@ -28,18 +27,22 @@ export const Phone = () => {
           minHeight: `${currentScreenSize.height}px`,
           background: template.background,
         }}
-        className={cn(`[&>*]:cursor-pointer h-full scrollbar scrollbar-hide rounded-5xl  border-[15px] border-gray-900 p-3 flex flex-col relative after:content-['']  after:absolute after:w-[100px] after:bg-black after:left-[35%] after:h-7 after:rounded-full  before:content-[''] before:absolute before:w-[140px] before:bg-white before:left-[28%] before:bottom-2 before:h-1 before:rounded-full`)}
+        className={cn(
+          `[&>*]:cursor-pointer h-full scrollbar scrollbar-hide rounded-5xl  border-[15px] border-gray-900 p-3 flex flex-col relative after:content-['']  after:absolute after:w-[100px] after:bg-black after:left-[35%] after:h-7 after:rounded-full  before:content-[''] before:absolute before:w-[140px] before:bg-white before:left-[28%] before:bottom-2 before:h-1 before:rounded-full`
+        )}
       >
-        <Droppable droppableId="s">
+        <Droppable droppableId="id">
           {(provided) => (
             <div
               ref={provided.innerRef}
-              className="mt-10"
+              className="mt-10 flex flex-col"
               {...provided.droppableProps}
             >
               <Template isPreview={false} />
+              {provided.placeholder}
             </div>
           )}
+          
         </Droppable>
       </div>
     </DragDropContext>
