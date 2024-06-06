6eaff6f39591d1e97bbed522c958b1f2977c785b
Ulugbek <ulugb3k270@gmail.com>
2024-06-06 11:38:04 +0500

    added routes on homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,19 @@
+'use client';
+import Link from 'next/link';
+
+import { availableRoutes } from '@/utils/mocks/login/routes.mock';
+
 export default function Home() {
-  return <h1 className="">Initial Project</h1>;
+  return (
+    <div className="flex flex-col ">
+      <h1>Available routes</h1>
+      {availableRoutes.map((route, index) => (
+        <div key={index} className='underline text-blue-400'>
+          <Link href={route} >
+            {route}
+          </Link>
+        </div>
+      ))}
+    </div>
+  );
 }
---
+++ utils/mocks/login/routes.mock.ts
@@ -0,0 +1,6 @@
+export const availableRoutes = [
+  '/home',
+  '/channel/:username',
+  '/auth/login',
+  '/auth/register',
+];
